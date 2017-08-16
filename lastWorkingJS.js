6fb5a227e3edea46ab8459ff3f51e8082c0a07b5
Fiaalii Togiaso <jtogiaso@gmail.com>
2017-08-16 03:48:37 -0700

    Non Object Oriented Commit

--- lastWorkingJS.js
+++ lastWorkingJS.js
@@ -1,34 +1,32 @@
 //Initializaing the arrays for the guesses
 var incorrectGuessArray = [];
+var rightGuessArray = [];
 var hiddenWordArray = "-";
 
 //
 var numberOfGuessesLeft = 10;
+var numberOfCorrectGuesses = 0;
 var numberOfLosses = 0;
 var numberOfWins = 0;
 
 //Initializing a word array, to which the other arrays will be compared
 var hangmanWord = "randomString";
-var listOfRandomWords = ["apple" , "turtle" , "buzzard" , "pseudonym" , "dog" , "cat" , "turkey" , "drink", "thanks" , "fortune" , "serfdom" , "country" , "flirt" , "snag" , "computer" , "herbivorous" , "hippopotamus" , "kinesiology" , "fraternity" , "leopard" , "zenzizenzizenzic" , "syzygy" , "synecdoche"];
+var listOfRandomWords = ["apple" , "turtle" , "buzzard" , "pseudonym" , "dog" , "cat" , "turkey" , "drink", "thanks" , "fortune" , "serfdom" , "country" , "flirt" , "snag" , "computer"];
 
 // capturing all of the elements we want to manipulate
 var failedGuessCenter = document.getElementById("failedGuessCenter");
 var hangmanPhrase = document.getElementById("hangmanPhrase");
+var correctGuessCenter = document.getElementById("correctGuessCenter");
 var instructionCenter = document.getElementById("instructionCenter");
 var winLossCenter = document.getElementById("winLossCenter");
+var numberOfLossesCenter = document.getElementById("numberOfLossesCenter");
 var numberOfGuessesLeftCenter = document.getElementById("numberOfGuessesLeftCenter");
-var hangmanState = document.getElementById("hangmanState");
-var imageHelp = document.getElementById("imageHelp");
-var helpCenter = document.getElementById("helpCenter");
-
-//Sounds
-var audioPlayer = document.getElementById("audioPlayer");
 
 
 //Any key will "Start" the game by sending the "user" to the newWord function
 document.onkeydown = function(letterKey) {
 	console.log(letterKey);
-	instructionCenter.textContent = "Press letter keys to guess and help Aang defeat Ozai!";
+	instructionCenter.textContent = "Have Fun! Press letter keys to guess the letters of the word!";
 	newWord();	
 }
 
@@ -41,13 +39,12 @@ function newWord () {
 	}
 	hangmanPhrase.textContent = hiddenWordArray;
 	incorrectGuessArray = [];
-	failedGuessCenter.textContent = "You have no failed guesses! ... yet";
-	numberOfGuessesLeft = Math.floor(hiddenWordArray.length * 1.5);
+	failedGuessCenter.textContent = "You have no guesses at all!";
+	rightGuessArray = [];
+	correctGuessCenter.textContent = "You have not correctly guessed anything!";
+	numberOfGuessesLeft = 10;
 	numberOfGuessesLeftCenter.textContent = "Number of guesses left: " + numberOfGuessesLeft;
 	numberOfCorrectGuesses = 0;
-	hangmanState.src = "assets/images/hangman1.png"
-	helpCenter.textContent = "Aang is energybending against Ozai. Help make sure Aang's spirit does not become corrupted!";
-	imageHelp.src = "assets/images/aangReadyHelp.gif";
 	gameLogic();
 
 }
@@ -61,8 +58,6 @@ function gameLogic () {
 		console.log(keyPress);
 
 		if (hangmanWord.indexOf(keyPress.key) === -1) {
-			audioPlayer.src = "assets/images/noYouIdiot.mp3";
-			audioPlayer.play();
 			if (incorrectGuessArray.indexOf(keyPress.key) === -1 && keyPress.keyCode >= 65 && keyPress.keyCode <= 90) {
 				incorrectGuessArray.push(keyPress.key);
 				incorrectGuessArray = incorrectGuessArray.join(" ");
@@ -70,11 +65,14 @@ function gameLogic () {
 				incorrectGuessArray = incorrectGuessArray.split("");
 				numberOfGuessesLeft--;
 				numberOfGuessesLeftCenter.textContent = "Number of guesses left: " + numberOfGuessesLeft;
-				hangmanStateChange();
 			}
 		}
 		else {
-			if (hiddenWordArray.indexOf(keyPress.key) === -1) {
+			if (rightGuessArray.indexOf(keyPress.key) === -1) {
+				rightGuessArray.push(keyPress.key);
+				rightGuessArray = rightGuessArray.join(" ");
+				correctGuessCenter.textContent = rightGuessArray;
+				rightGuessArray = rightGuessArray.split("");
 				for (var i = 0; i < hangmanWord.length; i++) {
 					if (keyPress.key === hangmanWord[i]) {
 						hiddenWordArray = hiddenWordArray.split("");
@@ -88,7 +86,7 @@ function gameLogic () {
 		}
 
 		if (keyPress.key === "Escape" || numberOfGuessesLeft === 0) {
-			numberOfGuessesLeftCenter.textContent = "You have failed all the world...";
+			numberOfGuessesLeftCenter.textContent = "No guesses remain!";
 			getOutOfOriginalOnKeyUpLose();
 		}
 		if (numberOfCorrectGuesses === hangmanWord.length) {
@@ -100,75 +98,36 @@ function gameLogic () {
 //If user loses, this function will be called and only pressing "Enter" will return user to the game. Also, number of losses is incremeneted.
 //Also moves the onkeyup element to this function in order to break 
 function getOutOfOriginalOnKeyUpLose () {
-	audioPlayer.src = "assets/images/thatsRoughBuddy.mp3";
-	audioPlayer.play();
-	instructionCenter.textContent = "You failed! The word was: " + hangmanWord + ". Press Enter to try again!";
-	numberOfLosses++;
-	winLossCenter.textContent = numberOfWins + " - " + numberOfLosses;
 	document.onkeydown = function(pressedKey) {
 		console.log("You pressed this button: " + pressedKey.key);
 		if (pressedKey.key === "Enter") {
-			instructionCenter.textContent = "Press letter keys to guess and help Aang defeat Ozai!";
+			instructionCenter.textContent = "Have Fun! Press letter keys to guess the letters of the word!";
 			newWord();
 		}
+
 	}
+	instructionCenter.textContent = "You Lost! The word was: " + hangmanWord + ". Press Enter to start over!";
+	numberOfLosses++;
+	winLossCenter.textContent = numberOfWins + " - " + numberOfLosses;
+
 }
 
 //If user wins, this function will be called. Instruction center div will also display a winning message
 function getOutOfOriginalOnKeyUpWin () {
-	winState();
-	instructionCenter.textContent = "You successfully helped Aang! Press Enter for another try!";
-	numberOfWins++;
-	winLossCenter.textContent = numberOfWins + " - " + numberOfLosses;
 	document.onkeydown = function(pressedKey) {
 		console.log("You pressed this button: " + pressedKey.key);
 		if (pressedKey.key === "Enter") {
-			instructionCenter.textContent = "Press letter keys to guess and help Aang defeat Ozai!";
+			instructionCenter.textContent = "Have Fun! Press letter keys to guess the letters of the word!";
 			newWord();
 		}
 	}
+	instructionCenter.textContent = "You win! Bet you can't do it again! Press Enter to start over!";
+	numberOfWins++;
+	winLossCenter.textContent = numberOfWins + " - " + numberOfLosses;
 
 }
 
 //Generate random integer between the provided the paramaters
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//Changes the picture based on number of guesses left
-function hangmanStateChange() {
-	if (numberOfGuessesLeft === 0) {
-		hangmanState.src = "assets/images/aangCorrupted.png";
-		imageHelp.src = "assets/images/AangDead.gif";
-		helpCenter.textContent = "Nice...";
-	}
-	else if (numberOfGuessesLeft === 1) {
-		hangmanState.src="assets/images/hangman5.gif";
-		imageHelp.src = "assets/images/sokkaPalm.gif";
-		helpCenter.textContent = "You have one last wrong guess! Do no let Aang be corrupted!";
-	}
-	else if (numberOfGuessesLeft < ((hangmanWord.length * 1.5) * (1/3))) {
-		hangmanState.src="assets/images/hangman4.gif";
-		imageHelp.src = "assets/images/nopeKick.gif";
-		helpCenter.textContent = "Geez! You are suppose to be helping...";
-	}
-	else if (numberOfGuessesLeft < ((hangmanWord.length * 1.5) * (2/3))) {
-		hangmanState.src="assets/images/hangman3.png";
-		imageHelp.src = "assets/images/aangPalm.gif";
-		helpCenter.textContent = "It's not looking to good for Aang!";
-	}
-	else if (numberOfGuessesLeft < (hangmanWord.length * 1.5)) {
-		hangmanState.src="assets/images/hangman2.jpg";
-		imageHelp.src = "assets/images/hahahaNO.gif";
-		helpCenter.textContent = "Aang's spirit is being corrupted! Hurry and guess the right word!";
-	}
-}
-
-function winState() {
-	hangmanState.src = "assets/images/aangHappy.gif";
-	imageHelp.src = "assets/images/celebration.gif";
-	helpCenter.textContent = "Nice!";
-	audioPlayer.src = "assets/images/nowCheckWin.mp3";
-	audioPlayer.play();
-
 }
